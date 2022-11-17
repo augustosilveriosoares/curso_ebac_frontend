f26e08c24039510ec7922da9dbb9cfaa5607a60a
Augusto Silverio Soares <falecomsilverio@gmail.com>
2022-11-17 18:09:36 -0300

    alter main js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $('form').on('submit',function(e){
     e.preventDefault();
    $('#lista').append( $('<li class="item"> ' + $('form input').val() +' </li>').hide().fadeIn(500));
    $('form input').val('');
-   $(document)
 })
 
 $("ul").on("click", "li", function(e){
